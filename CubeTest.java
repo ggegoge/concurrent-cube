08dd72601b58e4d3bea7b0037ded67470cb5f006
Grzegorz Cichosz <grzes.cichosz@gmail.com>
2021-11-26 18:37:43 +0100

    add CubeTest for junit

---
+++ CubeTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+public class CubeTest {
+    @Test
+    public void git() {
+        
+    }
+
+    @Test
+    public void chuj() {
+        throw new AssertionError("chuj");
+    }
+
+    @Test
+    public void cube1() {
+        Cube c = new Cube(3, (x, y) -> { System.out.println("before"); }, (x, y) -> { System.out.println("after");} () -> {}, () -> {});
+    }
+}
