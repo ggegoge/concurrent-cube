2dd34551e513299e218e54d26cf5295886bb50ad
Grzegorz Cichosz <grzes.cichosz@gmail.com>
2021-11-26 23:26:11 +0100

    add an interruption test

--- Chuj.java
+++ Chuj.java
@@ -2,26 +2,28 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.*;
 
 public class Chuj {
-        
+
     public static void order1260() {
-        var counter = new Object() { int value = 0; };
-        Cube cube = new Cube(10,
-                             (x, y) -> {
-                                 ++counter.value;
-                                 System.out.println("\tbefore rotating"); },
-                             (x, y) -> {
-                                 ++counter.value;
-                                 System.out.println("\tafter rotating"); },
-                             () -> {
-                                 ++counter.value;
-                                 System.out.println("\tbefore showing"); },
-                             () -> {
-                                 ++counter.value;
-                                 System.out.println("\tafter showing"); }
-                             );
+        var counter = new Object() {
+            int value = 0;
+        };
+        Cube cube = new Cube(10, (x, y) -> {
+            ++counter.value;
+            System.out.println("\tbefore rotating");
+        }, (x, y) -> {
+            ++counter.value;
+            System.out.println("\tafter rotating");
+        }, () -> {
+            ++counter.value;
+            System.out.println("\tbefore showing");
+        }, () -> {
+            ++counter.value;
+            System.out.println("\tafter showing");
+        });
 
         // 1260
         try {
@@ -39,8 +41,8 @@ public class Chuj {
             }
             System.out.println(cube.show());
         } catch (Exception e) {
-            
-        }        
+
+        }
     }
 
     private static int ax(int f) {
@@ -60,59 +62,63 @@ public class Chuj {
         int size = 10;
         int NR_THREADS = 100;
         List<String> log = Collections.synchronizedList(new ArrayList<>());
-        
-        Cube cube = new Cube(size,
-                             (x, y) -> { log.add("i" + ax(x)); },
-                             (x, y) -> { log.add("o" + ax(x)); },
-                             () -> { log.add("iS"); },
-                             () -> { log.add("oS");}
-                             );
+
+        Cube cube = new Cube(size, (x, y) -> {
+            log.add("i" + ax(x));
+        }, (x, y) -> {
+            log.add("o" + ax(x));
+        }, () -> {
+            log.add("iS");
+        }, () -> {
+            log.add("oS");
+        });
 
         List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < NR_THREADS; ++i) {
-            threads.add(new Thread(() -> {try {
-                            if (ThreadLocalRandom.current().nextDouble() < 0.3) {
-                                cube.show();
-                            } else {
-                                cube.rotate(ThreadLocalRandom.current().nextInt(0,2137) % 6,
-                                            ThreadLocalRandom.current().nextInt(0,2137) % size);
-                            }
-                        } catch (InterruptedException e) {
-                            // TODO
-                            e.printStackTrace();
-                        } }));
+            threads.add(new Thread(() -> {
+                try {
+                    if (ThreadLocalRandom.current().nextDouble() < 0.3) {
+                        cube.show();
+                    } else {
+                        cube.rotate(ThreadLocalRandom.current().nextInt(0, 2137) % 6,
+                                ThreadLocalRandom.current().nextInt(0, 2137) % size);
+                    }
+                } catch (InterruptedException e) {
+                    // TODO
+                    e.printStackTrace();
+                }
+            }));
         }
         threads.forEach(Thread::start);
 
         threads.forEach(arg0 -> {
-			try {
-				arg0.join();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		});
+            try {
+                arg0.join();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        });
 
         char current = log.get(0).charAt(1);
         int balance = 0;
-                        
+
         for (String state : log) {
             if (state.charAt(1) != current) {
                 if (balance != 0)
                     throw new AssertionError("found '" + state.charAt(1) + "' inside a block of '" + current + "'");
 
-                current = state.charAt(1);            
+                current = state.charAt(1);
             }
-            
+
             if (state.charAt(0) == 'i')
                 ++balance;
             else
                 --balance;
-            
-            if (balance < 0)
-                throw new AssertionError("negative i-o balance!");            
 
-                
+            if (balance < 0)
+                throw new AssertionError("negative i-o balance!");
+
         }
 
         if (balance != 0)
@@ -128,69 +134,112 @@ public class Chuj {
         try {
             String cubeString = cube.show();
             int expectedAmount = cubeString.length() / 6;
-            int occurences[] = {0, 0, 0, 0, 0, 0};
-            for (int i = 0; i < cubeString.length(); ++i) {
-                System.out.println(cubeString.charAt(i) + " = " + Character.getNumericValue(cubeString.charAt(i)));
+            int occurences[] = { 0, 0, 0, 0, 0, 0 };
+            for (int i = 0; i < cubeString.length(); ++i) {                
                 ++occurences[Character.getNumericValue(cubeString.charAt(i))];
             }
 
             for (int i = 0; i < 6; ++i) {
                 if (occurences[i] != expectedAmount) {
-                    throw new AssertionError("there are not enough occurences of colour '"
-                                             + i + "'; " + occurences[i] + ", expected " + expectedAmount);
+                    throw new AssertionError("there are not enough occurences of colour '" + i + "'; " + occurences[i]
+                            + ", expected " + expectedAmount);
                 }
             }
-            
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}        
+
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
-    
+
     public static void manyThreads() {
         int size = 10;
-        Cube cube = new Cube(size,
-                             (x, y) -> { },
-                             (x, y) -> { },
-                             () -> { },
-                             () -> { }
-                             );
-        
+        Cube cube = new Cube(size, (x, y) -> {
+        }, (x, y) -> {
+        }, () -> {
+        }, () -> {
+        });
+
         ArrayList<Thread> threads = new ArrayList<>();
         for (int i = 0; i < 1000; ++i) {
             final int a = i;
             System.out.println("a = " + a);
-            threads.add(new Thread(() -> {try {
-                            cube.rotate(0, a % size);                        
-                        } catch (InterruptedException e) {
-                            
-                        } }));
-            
-            threads.add(new Thread(() -> {try {
-                            cube.rotate(a % 5 + 1, 0);                        
-                        } catch (InterruptedException e) {
-                            
-                        } }));
+            threads.add(new Thread(() -> {
+                try {
+                    cube.rotate(0, a % size);
+                } catch (InterruptedException e) {
+
+                }
+            }));
+
+            threads.add(new Thread(() -> {
+                try {
+                    cube.rotate(a % 5 + 1, 0);
+                } catch (InterruptedException e) {
+
+                }
+            }));
         }
         threads.forEach(Thread::start);
         threads.forEach(arg0 -> {
-			try {
-				arg0.join();
-			} catch (InterruptedException e) {
-				System.err.println("INTERRUPTED!!!!!");
-			}
-		});
+            try {
+                arg0.join();
+            } catch (InterruptedException e) {
+                System.err.println("INTERRUPTED!!!!!");
+            }
+        });
         System.out.println("finito");
     }
 
-    
-    
+    public static void interruptionsTest() {
+        int size = 10;
+        int NR_THREADS = 100;
+        
+        Cube cube = new Cube(size, (x, y) -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ingored) { }
+        }, (x, y) -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ingored) { }
+        }, () -> { }, () -> { });
+
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < NR_THREADS; ++i) {
+            threads.add(new Thread(() -> {
+                try {
+                    if (ThreadLocalRandom.current().nextDouble() < 0.3) {
+                        cube.show();
+                    } else {
+                        cube.rotate(ThreadLocalRandom.current().nextInt(0, 2137) % 6,
+                                ThreadLocalRandom.current().nextInt(0, 2137) % size);
+                    }
+                } catch (InterruptedException interrupted) { }
+            }));
+        }
+        threads.forEach(Thread::start);
+
+        Random r = new Random();
+        for (int i = 0; i < threads.size(); i += r.nextInt(0, 7)) {
+            threads.get(i).interrupt();
+        }
+        threads.forEach(arg0 -> {
+			try {
+				arg0.join();
+			} catch (InterruptedException e) { }
+		});
+
+        assertCorrectCube(cube);
+    }
+
     public static void main(String[] args) {
-                    
+
         // order1260();
         // manyThreads();
-        loggingTest();
-
+        // loggingTest();
+        interruptionsTest();
+        
         // ExecutorService executor = Executors.newFixedThreadPool(10);
         // Future<Object> guwno = executor.submit(() -> cube.rotate(4, 0));
         // guwno.get();
